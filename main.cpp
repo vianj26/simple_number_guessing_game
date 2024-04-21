6a2022e90b43ccfd7115260370ae4e12fa72f600
ian_vergara <ian.vergara@gmail.com>
2024-04-21 21:04:54 +0800

    Added a feature that list all guesses and display it after the game

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
 void play_game();
+void print(vector<int>);
 
 int main()
 {
@@ -39,6 +41,7 @@ void play_game()
             int target = rand() % 101;
             int score = 10;
             int guess;
+            vector<int> guess_list;
 
             cout << "Guess a number from 0 to 100.\n>>";
             
@@ -46,20 +49,34 @@ void play_game()
             do
             {
                 cin >> guess;
+                guess_list.push_back(guess);
+
                 if(target > guess){cout << "Higher.\n>>";score--;}
                 else if( target < guess){cout << "Lower.\n>>";score--;}
 
-                if(score == 0){cout << "Sorry you lost.\n";}
+                if(score == 0){cout << "Sorry you lost.\n";print(guess_list);}
 
             } while (target != guess);
 
             cout << "Congratulations. Your score is " << score << ".\n";
+            print(guess_list);
             
         }
-
-        cout << "Do you want to play again?[y/n]\n>>";
+        
+        cout << endl << "Do you want to play again?[y/n]\n>>";
         cin >> answer; 
 
         if(!(answer == 'y' || answer == 'n')){cout << "Invalide response.\n";}
     } while (answer == 'y');
+}
+
+void print(vector<int> list)
+{
+    cout << "Guesses: [";
+    for(int n : list)
+    {
+        cout << n << ',';
+    }
+
+    cout << "\b]";
 }
